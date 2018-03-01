27688043a4f47cf26de2fdc04161b57fde7e6925
vrpgokultest <vrpgokulusa@gmail.com>
2018-03-01 11:29:41 -0800

    removed filestore

--- src/main/java/org/superbiz/moviefun/albums/AlbumsController.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsController.java
@@ -1,32 +1,22 @@
 package org.superbiz.moviefun.albums;
 
-import org.apache.tika.Tika;
 import org.apache.tika.io.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.superbiz.moviefun.blobstore.Blob;
 import org.superbiz.moviefun.blobstore.BlobStore;
-import org.superbiz.moviefun.blobstore.FileStore;
 
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 import java.util.Optional;
 
-import static java.lang.ClassLoader.getSystemResource;
 import static java.lang.String.format;
-import static java.nio.file.Files.readAllBytes;
 import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 import static org.springframework.http.MediaType.parseMediaType;
 
